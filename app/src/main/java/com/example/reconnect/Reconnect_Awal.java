170efd13a93ea7da0c3b43c3979e5c15856f94a2
dwiya azhari <dwiyaazhari56@gmail.com>
2021-09-29 09:15:08 -0700

    initial commit

---
+++ app/src/main/java/com/example/reconnect/Reconnect_Awal.java
@@ -0,0 +1,36 @@
+package com.example.reconnect;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Reconnect_Awal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setName(View view){
+        final TextView nameView = findViewById(R.id.nameView);
+        final EditText inputName = findViewById(R.id.inputname);
+        final String nameValue = inputName.getText().toString();
+        nameView.setText(nameValue);
+
+        final TextView emailView = findViewById(R.id.emailView);
+        final EditText inputEmail = findViewById(R.id.inputEmail);
+        final String EmailValue = inputEmail.getText().toString();
+        emailView.setText(EmailValue);
+
+        final TextView phoneView = findViewById(R.id.phoneView);
+        final EditText inputPhone = findViewById(R.id.inputPhone);
+        final String phoneValue = inputPhone.getText().toString();
+        phoneView.setText(phoneValue);
+
+    }
+
+}
---
+++ app/src/test/java/com/example/reconnect/Textview.java
@@ -0,0 +1,4 @@
+package com.example.reconnect;
+
+public class Textview {
+}
